4c77a8983788672e49126232d8cb316d6b1f291d
Zakariae Filali <filali.zakariae@gmail.com>
2017-05-14 02:32:18 +0100

    Add Authorizer entity

---
+++ Entity/Authorizer.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Kariae\AuthorizerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Authorizer
+ *
+ * @ORM\Table(name="authorizer")
+ * @ORM\Entity(repositoryClass="Kariae\AuthorizerBundle\Repository\AuthorizerRepository")
+ */
+class Authorizer
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="role", type="string", length=255)
+     */
+    private $role;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="controller", type="string", length=255)
+     */
+    private $controller;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="action", type="string", length=255)
+     */
+    private $action;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="vote", type="boolean")
+     */
+    private $vote;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set role
+     *
+     * @param string $role
+     *
+     * @return Authorizer
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return string
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Set controller
+     *
+     * @param string $controller
+     *
+     * @return Authorizer
+     */
+    public function setController($controller)
+    {
+        $this->controller = $controller;
+
+        return $this;
+    }
+
+    /**
+     * Get controller
+     *
+     * @return string
+     */
+    public function getController()
+    {
+        return $this->controller;
+    }
+
+    /**
+     * Set action
+     *
+     * @param string $action
+     *
+     * @return Authorizer
+     */
+    public function setAction($action)
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+
+    /**
+     * Get action
+     *
+     * @return string
+     */
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    /**
+     * Set vote
+     *
+     * @param boolean $vote
+     *
+     * @return Authorizer
+     */
+    public function setVote($vote)
+    {
+        $this->vote = $vote;
+
+        return $this;
+    }
+
+    /**
+     * Get vote
+     *
+     * @return bool
+     */
+    public function getVote()
+    {
+        return $this->vote;
+    }
+}
+
